8aa2bd6c50de73e625d0c473c2c71c9636eda490
Brian Akins <bakins@omniti.com>
2014-08-05 18:48:57 -0400

    more hackery

--- main.go
+++ main.go
@@ -85,15 +85,16 @@ type (
 	}
 
 	Domain struct {
-		XMLName struct{} `xml:"domain" json:"-"`
-		Type    string   `xml:"type,attr" json:"type"`
-		UUID    string   `xml:"uuid" json:"uuid"`
-		Name    string   `xml:"name" json:"name"`
-		Memory  int      `xml:"memory" json:"memory"`
-		VCPU    int      `xml:"vcpu" json:"vpcu"`
-		Devices Device   `xml:"devices,omitempty" json:"devices"`
-		Os      Os       `xml:"os,omitempty" json:"os"`
-		domain  *libvirt.VirDomain
+		*libvirt.VirDomain `xml:"-" json:"-"`
+		XMLName            struct{} `xml:"domain" json:"-"`
+		Type               string   `xml:"type,attr" json:"type"`
+		UUID               string   `xml:"uuid" json:"uuid"`
+		Name               string   `xml:"name" json:"name"`
+		Memory             int      `xml:"memory" json:"memory"`
+		VCPU               int      `xml:"vcpu" json:"vpcu"`
+		Devices            Device   `xml:"devices,omitempty" json:"devices"`
+		Os                 Os       `xml:"os,omitempty" json:"os"`
+		domain             *libvirt.VirDomain
 	}
 
 	ErrorMsg struct {
@@ -114,10 +115,10 @@ type (
 )
 
 func (d *Domain) Free() {
-	if d.Name != "" {
+	if d.VirDomain != nil {
 		fmt.Printf("Freeing: %s\n", d.Name)
-		d.domain.Free()
-		d.Name = ""
+		d.VirDomain.Free()
+		d.VirDomain = nil
 	}
 }
 
@@ -125,25 +126,19 @@ func (c *Context) FreeList(f Freer) {
 	c.freelist = append(c.freelist, f)
 }
 
-/*
-func (d *Domain) MarshalJSON() ([]byte, error) {
-	fmt.Printf("MarshalJSON: %s\n", d.Name)
-	return json.Marshal(d)
-}
-*/
 func buildDomain(dom *libvirt.VirDomain) (*Domain, error) {
-	xmldesc, err := dom.GetXMLDesc(0)
+	d := new(Domain)
+	d.VirDomain = dom
+
+	xmldesc, err := d.GetXMLDesc(0)
 	if err != nil {
 		return nil, err
 	}
-
-	d := new(Domain)
 	err = xml.Unmarshal([]byte(xmldesc), d)
 	if err != nil {
 		return nil, err
 	}
 
-	d.domain = dom
 	runtime.SetFinalizer(d, func(d *Domain) {
 		d.Free()
 	})
@@ -168,23 +163,57 @@ func listDomains(c *Context) error {
 	return nil
 }
 
-func getDomain(c *Context) error {
-	name := c.Params.ByName("name")
-	dom, err := c.V.LookupDomainByName(name)
+func getDomain(c *Context, d *Domain) error {
+	c.JSON(200, d)
+	return nil
+}
 
-	if err != nil {
-		code := 500
-		if strings.Contains(err.Error(), "Domain not found") {
-			code = 404
-		}
-		return c.JSONError(code, err)
-	}
-	d, err := buildDomain(&dom)
+// TODO: need a withDomain helper
+func destroyDomain(c *Context, d *Domain) error {
+	err := d.Destroy()
 	if err != nil {
 		return c.JSONError(500, err)
 	}
 	c.JSON(200, d)
 	return nil
+}
+
+func domainHandler(fn func(*Context, *Domain) error) gin.HandlerFunc {
+	return gin.HandlerFunc(func(c *gin.Context) {
+		v, err := libvirt.NewVirConnection("qemu:///system")
+		if err != nil {
+			c.Abort(500)
+		}
+		defer v.CloseConnection()
+		ctx := &Context{
+			Context:  c,
+			V:        &v,
+			freelist: make([]Freer, 0),
+		}
+		defer func() {
+			for _, f := range ctx.freelist {
+				f.Free()
+			}
+		}()
+		name := c.Params.ByName("name")
+		dom, err := v.LookupDomainByName(name)
+
+		if err != nil {
+			code := 500
+			if strings.Contains(err.Error(), "Domain not found") {
+				code = 404
+			}
+			ctx.JSONError(code, err)
+			return
+		}
+		d, err := buildDomain(&dom)
+		if err != nil {
+			ctx.JSONError(500, err)
+			return
+		}
+		defer d.Free()
+		fn(ctx, d)
+	})
 }
 
 func withContext(fn HandlerFunc) gin.HandlerFunc {
@@ -223,7 +252,9 @@ func main() {
 	domains := r.Group("/domains")
 	{
 		domains.GET("", withContext(listDomains))
-		domains.GET(":name", withContext(getDomain))
+		//domains.GET(":name", withContext(getDomain))
+		domains.GET(":name", domainHandler(getDomain))
+		domains.POST(":name/destroy", domainHandler(destroyDomain))
 	}
 
 	// Listen and server on 0.0.0.0:8080
